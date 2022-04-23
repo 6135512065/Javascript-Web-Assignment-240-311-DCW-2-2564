708c4a21504d8f3afc6d4c40f8e8090c01766e20
Your Name <lan_6744@hotmail.com>
2022-04-23 22:33:56 +0700

    commit6 (Edit LogoutPage)

--- frontend/pages/logout.js
+++ frontend/pages/logout.js
@@ -1,8 +1,5 @@
-import Head from 'next/head'
-import Layout from '../components/layout'
 import Navbar from '../components/navbar'
 import { useEffect, useState } from 'react'
-import styles from '../styles/Home.module.css'
 import axios from 'axios'
 import config from '../config/config'
 
@@ -19,19 +16,25 @@ export default function Logout({ token }) {
         let result = await axios.get(`${config.URL}/logout`, { withCredentials: true })
         setStatus("Logout successful")
     }
- 
+
     return (
-        <Layout>
-            <Head>
-                <title>User profile</title>
-            </Head>
-            <div className={styles.container}>
-                <Navbar />
-                <h1>Logout</h1>
-                <div>
-                    <h2> {status}  </h2>
+        <body class="bg-teal-100">
+            <Navbar />
+            <br></br><br></br><br></br><br></br><br></br><br></br>
+            <div class="flex justify-center">
+                <div class="rounded-lg shadow-lg bg-teal-300">
+                    <div class="p-6">
+                        <br></br>
+                        <h5 class="text-center text-teal-900 text-6xl font-medium mb-2">Logout Successful</h5>
+                        <div class="flex items-stretch justify-center px-6 py-8">
+                            <br></br>
+                            <button type="button" class=" inline-block px-6 py-2 bg-teal-900 text-white font-medium text-lg leading-tight uppercase rounded shadow-md hover:bg-teal-900 hover:shadow-lg focus:bg-teal-900 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-teal-500 active:shadow-lg transition duration-150 ease-in-out">
+                                {<a href='/'>กลับไปยังหน้าแรก</a>}
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </Layout>
+        </body>
     )
 }
